a3cf87679b56d228fe509ff2cfbaeed9a134915c
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-11-09 20:40:39 +0530

    d

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,51 +12,76 @@ terraform {
     }
   }
 
-  required_version = ">= 1.5.0"
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
   features {}
 }
 
-# === RANDOM SUFFIX ===
+############################################
+# VARIABLES
+############################################
+
+variable "prefix" {
+  default = "sentapi"
+}
+
+variable "location" {
+  default = "West Europe"  # ✅ Safe region
+}
+
+############################################
+# RESOURCE GROUP
+############################################
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+############################################
+# RANDOM SUFFIX
+############################################
+
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# === 1. RESOURCE GROUP (EXISTING) ===
-# Use data source since RG already exists
-data "azurerm_resource_group" "main" {
-  name = "sentimentapi-rg"
+############################################
+# STORAGE ACCOUNT (Required for Functions)
+############################################
+
+resource "azurerm_storage_account" "main" {
+  name                     = "${var.prefix}st${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-# === 2. CLIENT CONFIG ===
-data "azurerm_client_config" "current" {}
+############################################
+# APP SERVICE PLAN
+############################################
 
-# === 3. APP SERVICE PLAN ===
 resource "azurerm_service_plan" "main" {
   name                = "${var.prefix}-plan"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
   os_type             = "Linux"
-  sku_name            = "B1"  # Basic plan for Function App
+  sku_name            = "B1"     # ✅ Works in non-restricted regions
 }
 
-# === 4. STORAGE ACCOUNT ===
-resource "azurerm_storage_account" "main" {
-  name                             = "${var.prefix}st${random_id.suffix.hex}"
-  resource_group_name              = data.azurerm_resource_group.main.name
-  location                         = data.azurerm_resource_group.main.location
-  account_tier                     = "Standard"
-  account_replication_type         = "LRS"
-  allow_nested_items_to_be_public  = false
-}
+############################################
+# KEY VAULT
+############################################
 
-# === 5. KEY VAULT ===
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_key_vault" "main" {
   name                        = "${var.prefix}-kv"
-  location                    = data.azurerm_resource_group.main.location
-  resource_group_name          = data.azurerm_resource_group.main.name
+  location                    = var.location
+  resource_group_name          = azurerm_resource_group.main.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
 
@@ -62,36 +89,37 @@ resource "azurerm_key_vault" "main" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = [
-      "Get", "List", "Set", "Delete"
-    ]
+    secret_permissions = ["Get", "List", "Set"]
   }
-
-  depends_on = [data.azurerm_resource_group.main]
 }
 
-# === 6. SQL SERVER ===
+############################################
+# SQL SERVER + DB
+############################################
+
 resource "azurerm_mssql_server" "main" {
   name                         = "${var.prefix}-sqlsrv"
-  resource_group_name           = data.azurerm_resource_group.main.name
-  location                      = data.azurerm_resource_group.main.location
-  administrator_login           = "sqladminuser"
-  administrator_login_password  = "P@ssword1234!"
-  version                       = "12.0"
+  resource_group_name          = azurerm_resource_group.main.name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssword1234!"
 }
 
-# === 7. SQL DATABASE ===
 resource "azurerm_mssql_database" "main" {
   name      = "${var.prefix}-db"
   server_id = azurerm_mssql_server.main.id
   sku_name  = "S0"
 }
 
-# === 8. FUNCTION APP ===
+############################################
+# FUNCTION APP
+############################################
+
 resource "azurerm_linux_function_app" "main" {
   name                = "${var.prefix}-func"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
   service_plan_id     = azurerm_service_plan.main.id
   https_only          = true
 
@@ -109,11 +137,7 @@ resource "azurerm_linux_function_app" "main" {
   }
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
-    "AzureWebJobsStorage"      = azurerm_storage_account.main.primary_connection_string
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    AzureWebJobsStorage      = azurerm_storage_account.main.primary_connection_string
   }
-
-  depends_on = [
-    azurerm_storage_account.main
-  ]
 }
